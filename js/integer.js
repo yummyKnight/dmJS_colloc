017fd0419793978f7746d74e811675125815c82b
vsmaxim <vsmaxim8@gmail.com>
2018-04-16 14:52:06 +0300

    Добавлен класс для работы с целыми числами

---
+++ js/integer.js
@@ -0,0 +1,15 @@
+class Integer {
+    constructor(str) {
+        if (str[0] == '-') {
+            this.isNegative = true;
+            str = str.slice(1);
+        } else {
+            this.isNegative = false;
+        }
+        this.num = new Natural(str);
+    }
+    toString() {
+        return (this.isNegative) ? '-' + this.num : '' + this.num;
+    }
+}
+
