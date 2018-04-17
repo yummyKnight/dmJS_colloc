823a8ed07b4aad23d3f2e5ed123a7180cd2cd734
iWasBornInUSSR <sshedl22@gmail.com>
2018-04-17 18:43:44 +0300

    Тесты к натуралам
    ADD_NN_N
    SUB_NN_N
    MUL_Nk_N

--- test/specs/natural-specs.js
+++ test/specs/natural-specs.js
@@ -33,3 +33,37 @@ describe("ADD_1N_D", function() {
         assert.equal(ADD_1N_N(new Natural("123456")), "123457");
     });
 });
+
+describe("ADD_NN_N", function() {
+    it("999 + 1 = 1000", function(){
+        assert.equal(ADD_NN_N(new Natural("999"), new Natural("1")),1000);
+    });
+    it("45785147 + 24781447 = 70566594", function(){
+        assert.equal(ADD_NN_N(new Natural("45785147"), new Natural("24781447")),70566594);
+    });
+    it("7987987987854354 + 87987987987887987 = 95975975975742341", function(){
+        assert.equal(ADD_NN_N(new Natural("7987987987854354"), new Natural("87987987987887987")),95975975975742341);
+    });
+});
+describe("SUB_NN_N", function() {
+    it("999 - 999 = 0", function(){
+        assert.equal(SUB_NN_N(new Natural("999"), new Natural("999")),0);
+    });
+    it("45785147 - 24781447 = 21003700", function(){
+        assert.equal(SUB_NN_N(new Natural("45785147"), new Natural("24781447")),21003700);
+    });
+    it("87974848488789754 - 87974838488789754 = 10000000000", function(){
+        assert.equal(SUB_NN_N(new Natural("87974848488789754"), new Natural("87974838488789754")),10000000000);
+    });
+});
+describe("MUL_Nk_N", function() {
+    it("5 * 10 ^ 5 = 500000", function(){
+        assert.equal(MUL_Nk_N("5","5"),500000);
+    });
+    it("6487878 * 10 ^ 10 = 64878780000000000", function(){
+        assert.equal(MUL_Nk_N("6487878","10"),64878780000000000);
+    });
+    it("87974848488789754 * 10 ^ 17 = 8797484848878975400000000000000000", function(){
+        assert.equal(MUL_Nk_N("87974848488789754", "17"),8797484848878975400000000000000000);
+    });
+});
