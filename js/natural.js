c2b0c8863ec478f90153fa06eefb879cfd978e92
Eugene <shalugin1@mail.ru>
2018-04-17 17:32:28 +0300

    SUB_NN_N ; MUL_Nk_N
    Пашут при условии работы strip()

--- js/natural.js
+++ js/natural.js
@@ -7,6 +7,7 @@ class Natural {
     }
 }
 
+
 function COM_NN_D(first, second) {
     if (first.digits.length != second.digits.length) {
         return (first.digits.length > second.digits.length) ? 2 : 1;
@@ -52,34 +53,5 @@ function ADD_NN_N(first, second) {
 	return a;
 }	
 
-function SUB_NN_N(first, second) { 
-	let a = new Natural(first.toString()) 
-	let b = new Natural(second.toString()) 
-	if(COM_NN_D(a, b) == 0) 
-		return 0; 
-	else if(COM_NN_D(a, b) == 1) { 
-			b = new Natural(first.toString()) 
-			a = new Natural(second.toString()) 
-		} 
-	for(let i=0; i < a.digits.length; i++) { 
-		if(i < (a.digits.length - 1)) { 
-			a.digits[i + 1]--; 
-			a.digits[i] += 10; 
-		} 
-		if(i < b.digits.length) 
-			a.digits[i] -= b.digits[i]; 
-		if(a.digits[i] > 9) { 
-			a.digits[i + 1]++; 
-			a.digits[i] %= 10; 
-		} 
-	} 
-	a.strip();
-	return a; 
-}
 
-function MUL_Nk_N(first, k) {
-	for(let i = 0; i < k; i++)
-		first += 0;
-	first.strip();
-	return first;
-}
+		
