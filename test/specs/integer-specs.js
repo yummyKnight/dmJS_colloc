f340d2be42b88d28730123d68f9971f5983a4d6f
iWasBornInUSSR <sshedl22@gmail.com>
2018-04-19 19:24:57 +0300

    Исправил опечатку в функции, тесты для целых теперь работают, переписал вычитание целых.

--- test/specs/integer-specs.js
+++ test/specs/integer-specs.js
@@ -11,12 +11,12 @@ describe("ADD_ZZ_Z", function() {
 });
 describe("SUB_ZZ_Z", function() {
     it("999 - -1 = 1000", function(){
-        assert.equal(ADD_ZZ_Z(new Integer("999"), new Integer("-1")),1000);
+        assert.equal(SUB_ZZ_Z(new Integer("999"), new Integer("-1")), 1000);
     });
     it("-45785147 - 24781447 = -70566594", function(){
-        assert.equal(ADD_ZZ_Z(new Integer("-45785147"), new Integer("24781447")),-70566594);
+        assert.equal(SUB_ZZ_Z(new Integer("-45785147"), new Integer("24781447")),-70566594);
     });
     it("-7987987 - -8798798 = 810811", function(){
-        assert.equal(ADD_ZZ_Z(new Integer("-7987987"), new Integer("-8798798")),810811);
+        assert.equal(SUB_ZZ_Z(new Integer("-7987987"), new Integer("-8798798")), 810811);
     });
 });
